1167cc814663889717f5d1be30ec448b5fda7af6
B1lol-dev <sardorovichgamer@gmail.com>
2025-05-14 22:27:02 +0500

    initialize repo and configure all things

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { Home } from "./pages/Home/Home";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Navigate to="/" />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
