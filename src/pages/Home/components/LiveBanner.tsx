ae7a11e59445aae5fb19fb92ef67ed633ca42672
B1lol-dev <sardorovichgamer@gmail.com>
2025-05-15 15:26:16 +0500

    added translation to live banner in home page

--- src/pages/Home/components/LiveBanner.tsx
+++ src/pages/Home/components/LiveBanner.tsx
@@ -1,14 +1,21 @@
 import { Container } from "@/components/helpers/Container";
 import { Button } from "@/components/ui/button";
+import { useTranslation } from "react-i18next";
 
 export const LiveBanner = () => {
+  const [t] = useTranslation("translation");
+
   return (
     <section className="mt-[70px]">
       <Container>
         <div className="bg-ss-light-gray px-[90px] py-8">
-          <h1 className="text-5xl font-bold">Galaxy Unpacked Special</h1>
-          <h3 className="font-bold">Discover a bold new series</h3>
-          <Button className="rounded-full w-[113px] mt-4">Watch live</Button>
+          <h1 className="text-5xl font-bold">
+            {t("pages.home.live_banner.title")}
+          </h1>
+          <h3 className="font-bold">{t("pages.home.live_banner.text")}</h3>
+          <Button className="rounded-full min-w-[113px] mt-4">
+            {t("pages.home.live_banner.button")}
+          </Button>
         </div>
       </Container>
     </section>
